6fc1448ede0d0f3033c8db35cc79fa3b599cf00f
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-09-27 12:03:08 -0300

    tabela de users
    apenas listagem estatica

--- resources/js/Pages/Admin/Users.vue
+++ resources/js/Pages/Admin/Users.vue
@@ -1,12 +1,34 @@
 <template>
     <layout-dashboard>
-        <h2>USers</h2>
+        <simple-card title="Usuarios" class="w-100">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>2FA</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>User 1</td>
+                        <td>email@email.com</td>
+                        <td>Ativo</td>
+                    </tr>
+                    <tr>
+                        <td>User 1</td>
+                        <td>email@email.com</td>
+                        <td>Ativo</td>
+                    </tr>
+                </tbody>
+            </table>
+        </simple-card>
     </layout-dashboard>
 </template>
 <script>
-export default{
-    data(){
-        return{
+export default {
+    data() {
+        return {
 
         }
     },
