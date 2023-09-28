8b58c1cd5782c009aec5b4e5a30214d2db564cf6
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-09-28 11:16:19 -0300

    tabela finalizada estaticamente

--- resources/js/Pages/Admin/Users.vue
+++ resources/js/Pages/Admin/Users.vue
@@ -6,6 +6,7 @@
                     <tr>
                         <th>Nome</th>
                         <th>E-mail</th>
+                        <th>Ativo</th>
                         <th>2FA</th>
                     </tr>
                 </thead>
@@ -13,24 +14,47 @@
                     <tr>
                         <td>User 1</td>
                         <td>email@email.com</td>
-                        <td>Ativo</td>
+                        <td>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input custom-danger pointer" type="checkbox" role="switch" id="flexSwitchCheckChecked"
+                                    checked>
+                                <label class="form-check-label" for="flexSwitchCheckChecked">Ativo</label>
+                            </div>
+                        </td>
+                        <td>
+                            <button-load text="Desativado" class="btn btn-sm btn-outline-danger" :disable="true"></button-load>
+                        </td>
                     </tr>
                     <tr>
                         <td>User 1</td>
                         <td>email@email.com</td>
-                        <td>Ativo</td>
+                        <td>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input custom-danger pointer" type="checkbox" role="switch" id="flexSwitchCheckChecked"
+                                    >
+                                <label class="form-check-label" for="flexSwitchCheckChecked">Ativo</label>
+                            </div>
+                        </td>
+                        <td>
+                            <button-load text="Desativar" class="btn btn-sm btn-dark"></button-load>
+                        </td>
                     </tr>
                 </tbody>
             </table>
+            <div class="w-100 d-flex justify-content-end">
+                <!-- <paginate></paginate> -->
+
+            </div>
         </simple-card>
     </layout-dashboard>
 </template>
 <script>
+import { Paginate } from '../../components';
+
 export default {
     data() {
-        return {
-
-        }
+        return {};
     },
+    components: { Paginate }
 }
 </script>
