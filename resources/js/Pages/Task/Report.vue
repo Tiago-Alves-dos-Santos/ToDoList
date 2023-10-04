9ff0186d03c64cb4393024050ea246be93ded246
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-10-03 22:06:14 -0300

    campos de data adicionados

--- resources/js/Pages/Task/Report.vue
+++ resources/js/Pages/Task/Report.vue
@@ -1,6 +1,24 @@
 <template>
     <layout-dashboard>
-        <h3>report</h3>
+        <simple-card title="Imprimir PDF" sub_title="Selecione a data" class="w-100">
+            <form>
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="">Data inicio</label>
+                        <input type="date" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="">Data Fim</label>
+                        <input type="date" class="form-control">
+                    </div>
+                </div>
+                <div class="row mt-2">
+                    <div class="col-md-12 d-flex justify-content-end">
+                        <button-load icon="bi bi-filetype-pdf" class="btn btn-danger" title="Imprimir"></button-load>
+                    </div>
+                </div>
+            </form>
+        </simple-card>
     </layout-dashboard>
 </template>
 <script>
