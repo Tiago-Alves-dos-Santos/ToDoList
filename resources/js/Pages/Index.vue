12d6ff84275c82bd56dba05c0958211779402283
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-10-05 16:07:35 -0300

    mudança de cor principal

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,21 +1,13 @@
 <template>
     <layout-center>
         <div id="index">
-            <div class="conatiner-title-action">
-                <div class="body">
-                    <h1>Bem-Vindo ao ToDoList</h1>
-                    <h3>Gerencie tarefas de forma eficaz. Comece agora:</h3>
-                    <div class="action">
-                        <button-load text="Administrador" type="button" class="pink" @click="routeTo('admin')"></button-load>
-                        <button-load text="Cliente" type="button" class="purple" @click="routeTo('user')"></button-load>
-                    </div>
+            <div class="body">
+                <h1>Bem-Vindo ao ToDoList</h1>
+                <h3>Gerencie tarefas de forma eficaz. Comece agora:</h3>
+                <div class="action">
+                    <button-load text="Administrador" type="button" class="pink" @click="routeTo('admin')"></button-load>
+                    <button-load text="Cliente" type="button" class="purple" @click="routeTo('user')"></button-load>
                 </div>
-            </div>
-            <div class="text">
-                <p>
-                    Comece a transformar seu dia a dia. Com o ToDoList, você terá o controle total das suas tarefas.
-                    Simplifique sua vida e alcance suas metas com apenas alguns cliques.
-                </p>
             </div>
         </div>
     </layout-center>
@@ -33,10 +25,10 @@ export default {
         }
     },
     methods: {
-        routeTo(route){
+        routeTo(route) {
             // let url = this.$route(route);
             // router.get(url);
-            router.get('/'+route);
+            router.get('/' + route);
         }
     }
 }
