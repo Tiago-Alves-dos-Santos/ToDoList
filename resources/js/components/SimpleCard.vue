03de0300196aec5cef53f07e4efb92bf451142f1
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-10-05 16:31:06 -0300

    form responsive

--- resources/js/components/SimpleCard.vue
+++ resources/js/components/SimpleCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card" style="width: 25rem;">
+    <div class="card">
         <div class="card-body">
             <h5 class="card-title text-center">{{ title }}</h5>
             <h6 class="card-subtitle mb-2 text-body-secondary text-center">{{ sub_title }}</h6>
